3c08255e800ad01d5103f407c1714fcf30c631e8
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-11 11:45:26 +0300

    Add files and fragments

---
+++ app/src/main/java/com/example/bookarchice/ui/auth/ForgotPasswordFragment.kt
@@ -0,0 +1,26 @@
+package com.example.bookarchice.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookarchice.R
+import com.example.bookarchice.databinding.FragmentForgotPasswordBinding
+
+class ForgotPasswordFragment : Fragment() {
+
+    private lateinit var binding: FragmentForgotPasswordBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_forgot_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentForgotPasswordBinding.bind(view)
+    }
+}
---
+++ app/src/main/java/com/example/bookarchice/ui/auth/LoginFragment.kt
@@ -0,0 +1,27 @@
+package com.example.bookarchice.ui.auth
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookarchice.R
+import com.example.bookarchice.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentLoginBinding.bind(view)
+    }
+
+}
