aaad00fa69f35276961ea58f5fad17744c3dafd8
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-05-23 17:48:07 +0300

    Add home screen design

---
+++ app/src/main/java/com/example/bookarchice/ui/home/HomeFragment.kt
@@ -0,0 +1,27 @@
+package com.example.bookarchice.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.bookarchice.R
+import com.example.bookarchice.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+    }
+
+}
