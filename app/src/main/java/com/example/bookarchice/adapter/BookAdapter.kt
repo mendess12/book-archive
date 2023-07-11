aca9078a0ea9c11478a2a24b89324ed0e602a217
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-07-11 13:53:46 +0300

    Add click listener for recycler item

--- app/src/main/java/com/example/bookarchice/adapter/BookAdapter.kt
+++ app/src/main/java/com/example/bookarchice/adapter/BookAdapter.kt
@@ -4,26 +4,25 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 import com.example.bookarchice.R
 import com.example.bookarchice.model.Book
 
-class BookAdapter(private val bookList: List<Book>) :
+class BookAdapter(private val bookList: List<Book>,private val listener : Listener) :
     RecyclerView.Adapter<BookAdapter.MyViewHolder>() {
 
+    interface Listener {
+        fun onItemClick(bookList: Book)
+    }
+
     class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        fun bind(bookList: Book, listener: Listener) {
+            itemView.setOnClickListener {
+                listener.onItemClick(bookList)
+            }
+        }
         var bookName: TextView = view.findViewById(R.id.recycler_item_book_name)
         var bookAuthor: TextView = view.findViewById(R.id.recycler_item_book_author)
-        var pageNumber: TextView = view.findViewById(R.id.recycler_item_book_page_number)
-        var bookType: TextView = view.findViewById(R.id.recycler_item_book_type)
-        var bookLanguage: TextView = view.findViewById(R.id.recycler_item_book_language)
-        var bookPublisher: TextView = view.findViewById(R.id.recycler_item_book_publishing_house)
-        var bookMessage: TextView = view.findViewById(R.id.recycler_item_book_message)
-        var bookDate: TextView = view.findViewById(R.id.recycler_item_date)
-        var constraintLayout: ConstraintLayout = itemView.findViewById(R.id.constraint_one)
-        var expandableLayout: ConstraintLayout = itemView.findViewById(R.id.constraint_two)
-
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
@@ -38,33 +37,11 @@ class BookAdapter(private val bookList: List<Book>) :
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val book = bookList[position]
+        holder.bind(book,listener)
         holder.bookName.text = book.bookName
         holder.bookAuthor.text = book.bookAuthor
-        holder.pageNumber.text = book.pageNumber
-        holder.bookType.text = book.bookType
-        holder.bookLanguage.text = book.bookLanguage
-        holder.bookPublisher.text = book.bookPublisher
-        holder.bookMessage.text = book.bookMessage
-        holder.bookDate.text = book.bookDate
-
-        val isExpandable = book.expandable
-        holder.expandableLayout.visibility = if (isExpandable) View.VISIBLE else View.GONE
-
-        holder.constraintLayout.setOnClickListener {
-            isAnyItemExpanded(position)
-            book.expandable = !book.expandable
-            notifyItemChanged(position, Unit)
+        holder.itemView.setOnClickListener {
+            listener.onItemClick(book)
         }
     }
-
-    private fun isAnyItemExpanded(position: Int) {
-        val temp = bookList.indexOfFirst {
-            it.expandable
-        }
-        if (temp >= 0 && temp != position) {
-            bookList[temp].expandable = false
-            notifyItemChanged(temp, 0)
-        }
-    }
-
 }
