ac33f33e2e4f900a735c13d91a368f9ddc58b8c5
YUSUF\yusuf <yusufmendes123@gmail.com>
2023-08-14 13:24:04 +0300

    Forgot password method TODOS resolved

--- app/src/main/java/com/example/bookarchice/repository/AuthRepository.kt
+++ app/src/main/java/com/example/bookarchice/repository/AuthRepository.kt
@@ -1,7 +1,5 @@
 package com.example.bookarchice.repository
 
-import androidx.lifecycle.MutableLiveData
-import com.example.bookarchice.util.logDebug
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.tasks.await
@@ -11,10 +9,6 @@ import kotlinx.coroutines.tasks.await
 // TODO Task donmektense, coroutine extension kutuphanesi eklenecek
 class AuthRepository(private val firebaseAuth: FirebaseAuth) {
 
-    private val loginLogTitle = "Error Login"
-    private val registerLogTitle = "Error Register"
-    private val forgotPasswordLogTitle = "Error Forgot Password"
-
     suspend fun login(email: String, password: String): AuthResult{
         return firebaseAuth.signInWithEmailAndPassword(email, password).await()
     }
@@ -23,18 +17,7 @@ class AuthRepository(private val firebaseAuth: FirebaseAuth) {
         return firebaseAuth.createUserWithEmailAndPassword(email, password).await()
     }
 
-    fun forgotPassword(email: String, forgotPasswordLiveData: MutableLiveData<String>) {
-
-        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener {
-            if (it.isSuccessful) {
-                forgotPasswordLiveData.postValue(it.toString())
-            } else {
-                logDebug(forgotPasswordLogTitle, it.exception.toString())
-                forgotPasswordLiveData.postValue(null)
-            }
-        }.addOnFailureListener {
-            forgotPasswordLiveData.postValue(null)
-            logDebug(forgotPasswordLogTitle, it.localizedMessage!!)
-        }
+    suspend fun forgotPassword(email: String): Void {
+        return firebaseAuth.sendPasswordResetEmail(email).await()
     }
 }
