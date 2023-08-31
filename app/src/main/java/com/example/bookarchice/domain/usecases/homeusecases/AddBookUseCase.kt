8b10746dd3b4be7650235304799714827175c9b7
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-31 13:24:35 +0300

    Add use case for add book

---
+++ app/src/main/java/com/example/bookarchice/domain/usecases/homeusecases/AddBookUseCase.kt
@@ -0,0 +1,16 @@
+package com.example.bookarchice.domain.usecases.homeusecases
+
+import com.example.bookarchice.domain.SuspendUseCase
+import com.example.bookarchice.domain.repos.HomeRepository
+import com.example.bookarchice.model.Book
+import com.google.firebase.firestore.DocumentReference
+import javax.inject.Inject
+
+class AddBookUseCase @Inject constructor(private val homeRepository: HomeRepository) :
+    SuspendUseCase<AddBookParams, DocumentReference>() {
+    override suspend fun execute(params: AddBookParams): DocumentReference {
+        return homeRepository.addBookData(params.book)
+    }
+}
+
+data class AddBookParams(val book: Book)
--- app/src/main/java/com/example/bookarchice/ui/home/addbook/AddBookViewModel.kt
+++ app/src/main/java/com/example/bookarchice/ui/home/addbook/AddBookViewModel.kt
@@ -4,15 +4,16 @@ import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.bookarchice.domain.usecases.homeusecases.AddBookParams
+import com.example.bookarchice.domain.usecases.homeusecases.AddBookUseCase
 import com.example.bookarchice.model.Book
-import com.example.bookarchice.repository.HomeRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.lang.Exception
 import javax.inject.Inject
 
 @HiltViewModel
-class AddBookViewModel @Inject constructor(private val homeRepository: HomeRepository) :
+class AddBookViewModel @Inject constructor(private val addBookUseCase: AddBookUseCase) :
     ViewModel() {
 
     var addLiveData = MutableLiveData<String?>()
@@ -20,7 +21,7 @@ class AddBookViewModel @Inject constructor(private val homeRepository: HomeRepos
     fun addBookDataFromFirebase(book: Book) {
         viewModelScope.launch {
             try {
-                val result = homeRepository.addBookData(book)
+                val result = addBookUseCase(AddBookParams(book))
                 addLiveData.postValue(result.toString())
 
             } catch (exception: Exception) {
