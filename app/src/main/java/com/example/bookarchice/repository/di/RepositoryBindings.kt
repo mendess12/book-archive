9e002824bafb1d0a9d934cf5ef89a15ec74ac79e
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-31 13:24:08 +0300

    Add module for home repository

--- app/src/main/java/com/example/bookarchice/repository/di/RepositoryBindings.kt
+++ app/src/main/java/com/example/bookarchice/repository/di/RepositoryBindings.kt
@@ -1,7 +1,9 @@
 package com.example.bookarchice.repository.di
 
 import com.example.bookarchice.domain.repos.AuthRepository
+import com.example.bookarchice.domain.repos.HomeRepository
 import com.example.bookarchice.repository.AuthRepositoryImpl
+import com.example.bookarchice.repository.HomeRepositoryImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ abstract class RepositoryModule {
     @Singleton
     @Binds
     abstract fun bindAuthRepository(authRepositoryImpl: AuthRepositoryImpl): AuthRepository
+
+    @Singleton
+    @Binds
+    abstract fun bindHomeRepository(homeRepositoryImpl: HomeRepositoryImpl): HomeRepository
 }
