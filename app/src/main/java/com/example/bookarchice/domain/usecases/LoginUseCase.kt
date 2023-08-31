50f27f1b019f90017494104f1ceee2fde5620aae
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-08-31 13:20:29 +0300

    Add use case for login

---
+++ app/src/main/java/com/example/bookarchice/domain/usecases/LoginUseCase.kt
@@ -0,0 +1,18 @@
+package com.example.bookarchice.domain.usecases
+
+import com.example.bookarchice.domain.SuspendUseCase
+import com.example.bookarchice.domain.repos.AuthRepository
+import com.google.firebase.auth.AuthResult
+import javax.inject.Inject
+
+class LoginUseCase @Inject constructor(
+    private val authRepository: AuthRepository
+) :
+    SuspendUseCase<LoginParams, AuthResult>() {
+    override suspend fun execute(params: LoginParams): AuthResult {
+        return authRepository.login(params.email, params.password)
+    }
+
+}
+
+data class LoginParams(val email: String, val password: String)
--- app/src/main/java/com/example/bookarchice/ui/auth/login/LoginViewModel.kt
+++ app/src/main/java/com/example/bookarchice/ui/auth/login/LoginViewModel.kt
@@ -3,14 +3,15 @@ package com.example.bookarchice.ui.auth.login
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.example.bookarchice.domain.repos.AuthRepository
+import com.example.bookarchice.domain.usecases.LoginParams
+import com.example.bookarchice.domain.usecases.LoginUseCase
 import com.example.bookarchice.util.logDebug
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class LoginViewModel @Inject constructor(private val authRepository: AuthRepository) : ViewModel() {
+class LoginViewModel @Inject constructor(private val loginUseCase: LoginUseCase) : ViewModel() {
 
     private val loginLogTitle = "Error Login"
     var loginLiveData = MutableLiveData<String?>()
@@ -18,7 +19,7 @@ class LoginViewModel @Inject constructor(private val authRepository: AuthReposit
     fun getLoginDataFromRepository(email: String, password: String) {
         viewModelScope.launch {
             try {
-                val result = authRepository.login(email, password)
+                val result = loginUseCase(LoginParams(email, password))
                 loginLiveData.postValue(result.toString())
             } catch (exception: Exception) {
                 logDebug(loginLogTitle, exception.toString())
