138fd032e20cd5cc619880a917c633edd448a7d8
Yusuf Mendeş <yusufmendes123@gmail.com>
2023-09-04 19:17:55 +0300

    Add inject method for book use case

--- app/src/main/java/com/example/bookarchice/domain/usecases/home/GetBooksUseCase.kt
+++ app/src/main/java/com/example/bookarchice/domain/usecases/home/GetBooksUseCase.kt
@@ -3,8 +3,10 @@ package com.example.bookarchice.domain.usecases.home
 import com.example.bookarchice.domain.SuspendUseCase
 import com.example.bookarchice.domain.repos.HomeRepository
 import com.example.bookarchice.model.Book
+import javax.inject.Inject
 
-class GetBooksUseCase(private val homeRepository: HomeRepository): SuspendUseCase<Unit, List<Book>>() {
+class GetBooksUseCase @Inject constructor(private val homeRepository: HomeRepository) :
+    SuspendUseCase<Unit, List<Book>>() {
     override suspend fun execute(params: Unit): List<Book> {
         return homeRepository.getBookList()
     }
